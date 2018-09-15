d66352c9d52dcc17404418b214f6bdc7665321bc
MarcusThu <marcus.thuvesen@gmail.com>
2018-09-15 12:27:18 +0200

    listview

--- GeneratedPackList/ListView.swift
+++ GeneratedPackList/ListView.swift
@@ -57,22 +57,62 @@ class ListView: UIViewController, UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return businessObjects.count
+       
+        return sunBeachObjects.count + businessObjects.count
     }
+    
+    
+    var chosen1 = false
+    var chosen2 = false
+    var chosen3 = false
+    var chosen4 = false
+    var chosen5 = false
+    var chosen6 = false
+    var chosen7 = false
+    var chosen8 = false
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        let cell = tableView.dequeueReusableCell(withIdentifier: "IDENTIFIER", for: indexPath)
         
-        cell.textLabel?.text = businessObjects[indexPath.row]
-        cell.textLabel?.text = sunBeachObjects[indexPath.row]
-        cell.textLabel?.text = skiingObjects[indexPath.row]
-        cell.textLabel?.text = cityObjects[indexPath.row]
-        cell.textLabel?.text = hikingObjects[indexPath.row]
-        cell.textLabel?.text = businessObjects[indexPath.row]
-
-        return cell
         
+        if chosen1 == true{
+        print("Business print")
+        cell.textLabel?.text = businessObjects[indexPath.row]
+        }
+        if chosen2 == true{
+         print("sunBeach print")
+         cell.textLabel?.text = sunBeachObjects[indexPath.row]
+        }
+        if chosen3 == true {
+             print("Skiing print")
+        cell.textLabel?.text = skiingObjects[indexPath.row]
+        }
+        if chosen4 == true {
+             print("City print")
+        cell.textLabel?.text = cityObjects[indexPath.row]
+        }
+        
+        if chosen5 == true{
+        print("Hiking print")
+        cell.textLabel?.text = hikingObjects[indexPath.row]
+        }
+        if chosen6 == true {
+             print("Party print")
+        cell.textLabel?.text = partyObjects[indexPath.row]
+        }
        
+        if chosen7 == true {
+             print("training print")
+            cell.textLabel?.text = trainingObjects[indexPath.row]
+        }
+        
+        if chosen8 == true {
+             print("Business print")
+            cell.textLabel?.text = campingObjects[indexPath.row]
+        }
+        
+        return cell
+
     }
     
 }
--- GeneratedPackList/ReasonUI.swift
+++ GeneratedPackList/ReasonUI.swift
@@ -10,28 +10,14 @@ import UIKit
 
 class ReasonUI: UIViewController {
     
-   var businessActivity = false
+   
     var businessChosen = false   //Variable for color change
-    
-   var sunBeachActivity = false
     var sunBeachChosen = false   //Variable for color change
-    
-   var cityActivity = false
     var cityChosen = false   //Variable for color change
-    
-   var hikingActivity = false
     var hikingChosen = false   //Variable for color change
-    
-   var skiingActivity = false
     var skiingyChosen = false   //Variable for color change
-    
-   var trainingActivity = false
     var trainingChosen = false   //Variable for color change
-    
-   var partyActivity = false
     var partyChosen = false   //Variable for color change
-    
-   var campingActivity = false
     var campingChosen = false   //Variable for color change
     
     override func viewDidLoad() {
@@ -43,18 +29,24 @@ class ReasonUI: UIViewController {
     @IBOutlet weak var businessOutl: UIButton!
     @IBAction func businessBtn(_ sender: UIButton) {
        
+        
         if businessChosen == false {
-            businessActivity = true
-            print(businessActivity)
-            businessOutl.backgroundColor = UIColor.lightGray
             businessChosen = true
+            print(businessChosen)
+            businessOutl.backgroundColor = UIColor.lightGray
+            
+            let myVar = storyboard?.instantiateViewController(withIdentifier: "ListView") as! ListView
+            myVar.chosen1 = businessChosen
+            navigationController?.pushViewController(myVar, animated: true)
+           
         }
         else if businessChosen == true {
-            businessActivity = false
-            print(businessActivity)
-            businessOutl.backgroundColor = UIColor.black
             businessChosen = false
+            print(businessChosen)
+            businessOutl.backgroundColor = UIColor.black
         }
+        
+        
         
     }
     
@@ -62,19 +54,22 @@ class ReasonUI: UIViewController {
     @IBAction func sunBeachBtn(_ sender: UIButton) {
        
         if sunBeachChosen == false {
-            sunBeachActivity = true
-            print(sunBeachActivity)
-            sunBeachOutl.backgroundColor = UIColor.lightGray
             sunBeachChosen = true
+            print(sunBeachChosen)
+            sunBeachOutl.backgroundColor = UIColor.lightGray
+            let myVar2 = storyboard?.instantiateViewController(withIdentifier: "ListView") as! ListView
+            myVar2.chosen2 = sunBeachChosen
+            navigationController?.pushViewController(myVar2, animated: true)
+            
+            
+            
         }
         else if sunBeachChosen == true {
-            sunBeachActivity = false
-            print(sunBeachActivity)
-            sunBeachOutl.backgroundColor = UIColor.black
             sunBeachChosen = false
+            print(sunBeachChosen)
+            sunBeachOutl.backgroundColor = UIColor.black
         }
     }
-    
     
     
 
