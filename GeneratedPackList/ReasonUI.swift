b28127ce6244e3ee1d09f87ce3915b5fda60d402
MarcusThu <marcus.thuvesen@gmail.com>
2018-10-01 13:06:13 +0200

    Reason UI Autolayout ny funkar

--- GeneratedPackList/ReasonUI.swift
+++ GeneratedPackList/ReasonUI.swift
@@ -20,14 +20,16 @@ class ReasonUI: UIViewController{
     @IBOutlet weak var campingOutl: UIButton!
     
     
-    var businessChosen = false//Variable for color change
-    var sunBeachChosen = false   //Variable for color change
-    var cityChosen = false   //Variable for color change
-    var hikingChosen = false   //Variable for color change
-    var skiingChosen = false   //Variable for color change
-    var trainingChosen = false   //Variable for color change
-    var partyChosen = false   //Variable for color change
-    var campingChosen = false   //Variable for color change
+    //Variable for color change
+    var clickedBtn1 = false
+    var clickedBtn2 = false
+    var clickedBtn3 = false
+    var clickedBtn4 = false
+    var clickedBtn5 = false
+    var clickedBtn6 = false
+    var clickedBtn7 = false
+    var clickedBtn8 = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,74 +39,97 @@ class ReasonUI: UIViewController{
     
     
     @IBAction func chosenActivities(_ sender: UIButton) {
-        if sender.tag == 1 && {
+        
+        //If user clicks change BGColor
+        if sender.tag == 1 && clickedBtn1 == false {
             businessOutl.backgroundColor = UIColor.gray
+            clickedBtn1 = true
         }
-        if sender.tag == 2 {
+        else if sender.tag == 1 && clickedBtn1 == true{
+            businessOutl.backgroundColor = UIColor.white
+            clickedBtn1 = false
+        }
+        
+        if sender.tag == 2 && clickedBtn2 == false{
             beachOutl.backgroundColor = UIColor.gray
+            clickedBtn2 = true
         }
-        if sender.tag == 3 {
+        else if sender.tag == 2 && clickedBtn2 == true{
+            beachOutl.backgroundColor = UIColor.white
+            clickedBtn2 = false
+        }
+        
+        if sender.tag == 3 && clickedBtn3 == false{
             cityOutl.backgroundColor = UIColor.gray
+            clickedBtn3 = true
         }
-        if sender.tag == 4 {
+        else if sender.tag == 3 && clickedBtn3 == true{
+            cityOutl.backgroundColor = UIColor.white
+            clickedBtn3 = false
+        }
+        
+        if sender.tag == 4 && clickedBtn4 == false{
             skiingOutl.backgroundColor = UIColor.gray
+            clickedBtn4 = true
         }
-        if sender.tag == 5 {
+        else if sender.tag == 4 && clickedBtn4 == true{
+            skiingOutl.backgroundColor = UIColor.white
+            clickedBtn4 = false
+        }
+        
+        if sender.tag == 5 && clickedBtn5 == false{
             trainingOutl.backgroundColor = UIColor.gray
+            clickedBtn5 = true
         }
-        if sender.tag == 6 {
+        else if sender.tag == 5 && clickedBtn5 == true{
+            trainingOutl.backgroundColor = UIColor.white
+            clickedBtn5 = false
+        }
+        
+        if sender.tag == 6 && clickedBtn6 == false{
             partyOutl.backgroundColor = UIColor.gray
+            clickedBtn6 = true
         }
-        if sender.tag == 7 {
+        else if sender.tag == 6 && clickedBtn6 == true{
+            partyOutl.backgroundColor = UIColor.white
+            clickedBtn6 = false
+        }
+    
+        if sender.tag == 7 && clickedBtn7 == false{
             hikingOutl.backgroundColor = UIColor.gray
+            clickedBtn7 = true
         }
-        if sender.tag == 8 {
+        else if sender.tag == 7 && clickedBtn7 == true{
+            hikingOutl.backgroundColor = UIColor.white
+            clickedBtn7 = false
+        }
+    
+        if sender.tag == 8 && clickedBtn8 == false{
             campingOutl.backgroundColor = UIColor.gray
+            clickedBtn8 = true
+        }
+        else if sender.tag == 8 && clickedBtn8 == true{
+            campingOutl.backgroundColor = UIColor.white
+            clickedBtn8 = false
         }
     }
     
-    
-    
-    
-  
-   
-    
-    
  
-   /*
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {  //Sending values when user clicks next
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         var myVar = storyboard.instantiateViewController(withIdentifier: "ListView") as! ListView
-        myVar.chosen1 = businessChosen
-        myVar.chosen2 = sunBeachChosen
-        myVar.chosen3 = cityChosen
-        myVar.chosen4 = hikingChosen
-        myVar.chosen5 = skiingChosen
-        myVar.chosen6 = trainingChosen
-        myVar.chosen7 = partyChosen
-        myVar.chosen8 = campingChosen
+        myVar.chosen1 = clickedBtn1
+        myVar.chosen2 = clickedBtn2
+        myVar.chosen3 = clickedBtn3
+        myVar.chosen4 = clickedBtn4
+        myVar.chosen5 = clickedBtn5
+        myVar.chosen6 = clickedBtn6
+        myVar.chosen7 = clickedBtn7
+        myVar.chosen8 = clickedBtn8
         
         self.present(myVar, animated: true)
         
     }
   
-    */
-   
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
 }
