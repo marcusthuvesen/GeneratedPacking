cc852a0ecfc4a73b3914f47205410dd761a1ca99
MarcusThu <marcus.thuvesen@gmail.com>
2018-10-01 12:47:45 +0200

    autlayout reason

--- GeneratedPackList/ReasonUI.swift
+++ GeneratedPackList/ReasonUI.swift
@@ -8,9 +8,18 @@
 
 import UIKit
 
-class ReasonUI: UIViewController {
-    
+class ReasonUI: UIViewController{
    
+    @IBOutlet weak var businessOutl: UIButton!
+    @IBOutlet weak var beachOutl: UIButton!
+    @IBOutlet weak var cityOutl: UIButton!
+    @IBOutlet weak var skiingOutl: UIButton!
+    @IBOutlet weak var trainingOutl: UIButton!
+    @IBOutlet weak var partyOutl: UIButton!
+    @IBOutlet weak var hikingOutl: UIButton!
+    @IBOutlet weak var campingOutl: UIButton!
+    
+    
     var businessChosen = false//Variable for color change
     var sunBeachChosen = false   //Variable for color change
     var cityChosen = false   //Variable for color change
@@ -26,135 +35,43 @@ class ReasonUI: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBOutlet weak var businessOutl: UIButton!
-    @IBAction func businessBtn(_ sender: UIButton) {
-       
-        
-        if businessChosen == false {
-            businessChosen = true
-            print("Nu försöker vi skicka true till Listview")
-            businessOutl.backgroundColor = UIColor.lightGray
-            
-            
+    
+    @IBAction func chosenActivities(_ sender: UIButton) {
+        if sender.tag == 1 && {
+            businessOutl.backgroundColor = UIColor.gray
         }
-        else if businessChosen == true {
-            businessChosen = false
-            print("Nu skickar vi inget")
-            businessOutl.backgroundColor = UIColor.black
+        if sender.tag == 2 {
+            beachOutl.backgroundColor = UIColor.gray
         }
-        
+        if sender.tag == 3 {
+            cityOutl.backgroundColor = UIColor.gray
+        }
+        if sender.tag == 4 {
+            skiingOutl.backgroundColor = UIColor.gray
+        }
+        if sender.tag == 5 {
+            trainingOutl.backgroundColor = UIColor.gray
+        }
+        if sender.tag == 6 {
+            partyOutl.backgroundColor = UIColor.gray
+        }
+        if sender.tag == 7 {
+            hikingOutl.backgroundColor = UIColor.gray
+        }
+        if sender.tag == 8 {
+            campingOutl.backgroundColor = UIColor.gray
+        }
     }
+    
+    
+    
+    
+  
    
-   
-    
-    @IBOutlet weak var sunBeachOutl: UIButton!
-    @IBAction func sunBeachBtn(_ sender: UIButton) {
-       
-        if sunBeachChosen == false {
-            sunBeachChosen = true
-            print("Nu försöker vi skicka true2 till ListView")
-            sunBeachOutl.backgroundColor = UIColor.lightGray
-            
-        }
-        else if sunBeachChosen == true {
-            sunBeachChosen = false
-            print("Nu skickar vi inget2")
-            sunBeachOutl.backgroundColor = UIColor.black
-        }
-    }
     
     
-    @IBOutlet weak var cityOutl: UIButton!
-    @IBAction func cityBtn(_ sender: UIButton) {
-        if cityChosen == false {
-            cityChosen = true
-            print("Nu försöker vi skicka true2 till ListView")
-            cityOutl.backgroundColor = UIColor.lightGray
-            
-        }
-        else if cityChosen == true {
-            cityChosen = false
-            print("Nu skickar vi inget2")
-            cityOutl.backgroundColor = UIColor.black
-        }
-    }
-    
-    @IBOutlet weak var hikingOutl: UIButton!
-    @IBAction func hikingBtn(_ sender: UIButton) {
-        if hikingChosen == false {
-            hikingChosen = true
-            print("Nu försöker vi skicka true2 till ListView")
-            hikingOutl.backgroundColor = UIColor.lightGray
-            
-        }
-        else if hikingChosen == true {
-            hikingChosen = false
-            print("Nu skickar vi inget2")
-            hikingOutl.backgroundColor = UIColor.black
-        }
-    }
-    
-    @IBOutlet weak var skiingOutl: UIButton!
-    @IBAction func skiingBtn(_ sender: UIButton) {
-        if skiingChosen == false {
-            skiingChosen = true
-            print("Nu försöker vi skicka true2 till ListView")
-            skiingOutl.backgroundColor = UIColor.lightGray
-            
-        }
-        else if skiingChosen == true {
-            skiingChosen = false
-            print("Nu skickar vi inget2")
-            skiingOutl.backgroundColor = UIColor.black
-        }
-    }
-    
-    @IBOutlet weak var trainingOutl: UIButton!
-    @IBAction func trainingBtn(_ sender: UIButton) {
-        if trainingChosen == false {
-            trainingChosen = true
-            print("Nu försöker vi skicka true2 till ListView")
-            trainingOutl.backgroundColor = UIColor.lightGray
-            
-        }
-        else if trainingChosen == true {
-            trainingChosen = false
-            print("Nu skickar vi inget2")
-            trainingOutl.backgroundColor = UIColor.black
-        }
-    }
-    
-    @IBOutlet weak var partyOutl: UIButton!
-    @IBAction func partyBtn(_ sender: UIButton) {
-        if partyChosen == false {
-            partyChosen = true
-            print("Nu försöker vi skicka true2 till ListView")
-            partyOutl.backgroundColor = UIColor.lightGray
-            
-        }
-        else if partyChosen == true {
-            partyChosen = false
-            print("Nu skickar vi inget2")
-            partyOutl.backgroundColor = UIColor.black
-        }
-    }
-    
-    @IBOutlet weak var campingOutl: UIButton!
-    @IBAction func campingBtn(_ sender: UIButton) {
-        if campingChosen == false {
-            campingChosen = true
-            print("Nu försöker vi skicka true2 till ListView")
-            campingOutl.backgroundColor = UIColor.lightGray
-            
-        }
-        else if campingChosen == true {
-            campingChosen = false
-            print("Nu skickar vi inget2")
-            campingOutl.backgroundColor = UIColor.black
-        }
-    }
-    
-    
+ 
+   /*
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {  //Sending values when user clicks next
         
@@ -173,7 +90,7 @@ class ReasonUI: UIViewController {
         
     }
   
-    
+    */
    
     
     
