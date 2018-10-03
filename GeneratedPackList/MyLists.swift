43cd21c66c57612f095244d888694aa52c96d857
MarcusThu <marcus.thuvesen@gmail.com>
2018-10-03 23:44:28 +0200

    Nytt fönster fungerande tableview

---
+++ GeneratedPackList/MyLists.swift
@@ -0,0 +1,29 @@
+//
+//  MyLists.swift
+//  GeneratedPackList
+//
+//  Created by Marcus Thuvesen on 2018-10-03.
+//  Copyright © 2018 Marcus Thuvesen. All rights reserved.
+//
+
+import UIKit
+
+class MyLists: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IDENTIFIER2", for: indexPath)
+        return cell
+    }
+    
+}
