1354fc7fffbb48be03d13a8812489cea444a86cc
Marcus thuvesen <maci@marcuss-imac.lan>
2018-10-31 09:27:32 +0100

    upp weather

--- GeneratedPackList/WhatClimate.swift
+++ GeneratedPackList/WhatClimate.swift
@@ -9,14 +9,59 @@
 import UIKit
 
 class WhatClimate: UIViewController {
-
+    @IBOutlet weak var hotOutl: UIButton!
+    @IBOutlet weak var mildOutl: UIButton!
+    @IBOutlet weak var coldOutl: UIButton!
+    @IBOutlet weak var rainyOutl: UIButton!
+    
+    var clickedBtn1 = false
+    var clickedBtn2 = false
+    var clickedBtn3 = false
+    var clickedBtn4 = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func chosenWeather(_ sender: UIButton) {
+        //Change color if user clicks
+        
+        if sender.tag == 1 && clickedBtn1 == false {
+            hotOutl.alpha = 0.5
+            clickedBtn1 = true
+        }
+        else if sender.tag == 1 && clickedBtn1 == true{
+            hotOutl.alpha = 1
+            clickedBtn1 = false
+        }
+        if sender.tag == 2 && clickedBtn2 == false{
+            mildOutl.alpha = 0.5
+            clickedBtn2 = true
+        }
+        else if sender.tag == 2 && clickedBtn2 == true{
+            mildOutl.alpha = 1
+            clickedBtn2 = false
+        }
+        if sender.tag == 3 && clickedBtn3 == false{
+            coldOutl.alpha = 0.5
+            clickedBtn3 = true
+        }
+        else if sender.tag == 3 && clickedBtn3 == true{
+            coldOutl.alpha = 1
+            clickedBtn3 = false
+        }
+        if sender.tag == 4 && clickedBtn4 == false{
+            rainyOutl.alpha = 0.5
+            clickedBtn4 = true
+        }
+        else if sender.tag == 4 && clickedBtn4 == true{
+            rainyOutl.alpha = 1
+            clickedBtn4 = false
+        }
+    }
+    
     
     
 
